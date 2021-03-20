f005f89dbaf2da47fb3ce57409dda5bf92b8190d
LenoreYang <80328490+LenoreYang@users.noreply.github.com>
2021-03-20 22:01:50 +0800

    Add files via upload

---
+++ Homework20210316.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+'''
+Created on Tue Mar 16 20:20:48 2021
+
+@author: ILA
+'''
+# Homework No.1
+
+for i in range(5,0,-1):
+        print('*'*i)
+for a in range(2,6):
+        print('*'*a)
+  
+# Homework No.2
+
+qq=int(input('請輸入範圍起始值:'))
+ww=int(input('請輸入範圍結束值:'))
+
+
+for i in range(qq,ww+1):
+    if i %4!=0:
+        continue
+    print(i,'為4的倍數')
+
+
+
+# Homework No.3 終極密碼
+
+
